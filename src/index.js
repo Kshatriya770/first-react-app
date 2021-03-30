044a4a4fd075ad354d92f72c1f8788f4ba8cdf59
5amS3piol <Saurabh.s.kshatriya7@gmail.com>
2021-03-30 13:14:59 +0530

    buttons

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "./style.css"
 
-function App(){
-    return(
-        <div>
-        <h1 style= {{textAlign:"center"}}>Hello World </h1>
-        <button className ="button">click me</button></div>
-    );
-}
+import App from "./App"
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'))
